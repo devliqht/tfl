a499c6e11f537cf1357cd724500c3ef066666ba0
Matt Cabarrubias <monapwc2020@gmail.com>
2025-02-05 00:30:11 +0800

    ADD: Suspense to pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./Sidebar";
+import { Suspense } from "react";
+import SuspenseLoader from "./SuspenseLoader";
 
 const Layout = () => {
   return (
@@ -7,7 +9,9 @@ const Layout = () => {
       <div className="flex md:flex-row flex-col mt-5 md:mt-0">
           <Sidebar />
             <main className="flex-1 overflow-y-auto px-6 mt-16 md:m-0 md:p-8">
+            <Suspense fallback={<SuspenseLoader />}>
               <Outlet />
+            </Suspense>
             </main>
       </div>
     </>
