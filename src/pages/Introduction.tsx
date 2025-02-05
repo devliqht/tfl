f92b6fc05489d509a5b5dbde1387c61ac2489c23
Matt Cabarrubias <monapwc2020@gmail.com>
2025-02-05 21:08:59 +0800

    ADD: Derick and qrcode

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { AnimatedContainer } from "@/components/Containers";
+import { useTrail } from "@react-spring/web"
 
 const teamMembers = [
   { name: "Matt Erron Cabarrubias", img: "/matt.jpg" },
@@ -7,12 +9,19 @@ const teamMembers = [
   { name: "Simon Gabriel Gementiza", img: "/simon.jpg" },
   { name: "Rean Coyacot", img: "/rean.jpg" },
   { name: "Atheena Arcena", img: "/atheena.jpg" },
-  { name: "Derick Angelo Yu", img: "/jhanell.jpg" },
+  { name: "Derick Angelo Yu", img: "/derick.jpg" },
   { name: "Cris Lawrence Lucero", img: "/cris.jpg" },
   { name: "Jemuel Valencia", img: "/jemuel.jpg" },
 ];
 
 const Introduction: React.FC = () => {
+    const trail = useTrail(teamMembers.length, {
+        from: { opacity: 0, transform: "translateY(20px)" },
+        to: { opacity: 1, transform: "translateY(0px)" },
+        delay: 300, // Initial delay before animation starts
+        config: { tension: 200, friction: 20 },
+      });
+    
   return (
     <div className="min-h-screen pb-12 pt-0 md:pt-6">
       <div className="max-w-5xl mx-auto px-6">
@@ -22,9 +31,10 @@ const Introduction: React.FC = () => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10">
           {teamMembers.map((member, index) => (
+            <AnimatedContainer delay={index * 55}>
             <div
               key={index}
-              className="bg-gray-200 shadow-lg rounded-lg p-6 flex flex-col items-center transition-transform transform hover:scale-105"
+              className="bg-gray-200 shadow-lg rounded-lg p-6 flex flex-col h-full items-center transition-transform transform hover:scale-105"
             >
               <div className="w-24 h-24 max-w-32 max-h-32 bg-gray-200 text-white flex items-center justify-center text-xl font-bold rounded-full">
                 <img src={member.img} alt="Img" className="w-full h-full object-cover rounded-full" />
@@ -33,8 +43,12 @@ const Introduction: React.FC = () => {
                 {member.name}
               </h3>
             </div>
+            </AnimatedContainer>
           ))}
         </div>
+        <AnimatedContainer delay={500}>
+        <img className="h-auto w-96 mx-auto" src="/qr.png" />
+        </AnimatedContainer>
       </div>
     </div>
   );
